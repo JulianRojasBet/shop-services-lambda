2f4e2e443df2977eb2361e904d1b6424840dc0db
Julian Rojas <julian.rojas@partners.goodrx.com>
2022-07-24 13:12:17 -0500

    [Task 4] Add createProduct service

---
+++ product-service/functions/createProduct/handler.ts
@@ -0,0 +1,44 @@
+import { sql } from "database";
+import { Product } from "types/Product";
+
+type Event = { body: Product & { count: number } };
+
+export const createProduct = async (event: Event) => {
+  const { id, title, description, price, count } = event.body || {};
+  console.log('new createProduct request with:', event.body);
+  const headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "OPTIONS,GET,POST",
+  };
+
+  try {
+    const [product, stock] = await sql.begin(async (sql) => {
+      const [product] = await sql`
+        INSERT INTO product (id, title, description, price)
+        VALUES (${id}, ${title}, ${description}, ${price}) RETURNING *;
+      `;
+      const [stock] = await sql`
+        INSERT INTO stock (product_id, count)
+        VALUES (${product.id}, ${count}) RETURNING *;
+      `;
+      return [product, stock];
+    });
+  
+    return {
+      headers,
+      statusCode: 200,
+      body: JSON.stringify({ ...product, count: stock.count }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ error: "Internal server error" }),
+    };
+  } finally {
+    sql.end();
+  }
+};
